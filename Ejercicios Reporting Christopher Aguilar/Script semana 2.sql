d790802888f425e2e4a84a40175ab0a8de0b19e2
AlexProMaster <achristopher231@gmail.com>
2023-06-14 11:45:58 -0600

    Mis reportes

--- Ejercicios Reporting Christopher Aguilar/Script semana 2.sql
+++ Ejercicios Reporting Christopher Aguilar/Script semana 2.sql
@@ -86,24 +86,37 @@ END
 /*Consultas Semana 2*/
 
 /*Ejercicio 1*/
+GO
+CREATE OR ALTER PROCEDURE UDP_Ejercicio1
+@Product_Id INT
+AS
+BEGIN
 SELECT prod.ProductID, 
 prod.Name, 
 prodcategory.Name AS ProductCategory,
 prodsubcategory.Name AS ProductSubCategory,
 prodmodel.Name AS ProductName,
-prodanddescription.Description
-
+prodanddescription.Description,
+prod.DiscontinuedDate
 FROM [Production].[Product] prod INNER JOIN [Production].[ProductSubcategory] prodsubcategory
 ON prod.ProductSubcategoryID = prodsubcategory.ProductSubcategoryID INNER JOIN [Production].[ProductCategory] prodcategory
 ON prodsubcategory.ProductCategoryID = prodcategory.ProductCategoryID INNER JOIN [Production].[ProductModel] prodmodel
 ON prod.ProductModelID = prodmodel.ProductModelID INNER JOIN [Production].[vProductAndDescription] prodanddescription
 ON prod.ProductID = prodanddescription.ProductID
-WHERE prodanddescription.CultureID = 'en' AND prodsubcategory.Name IN ('Road Frames','Mountain Bikes','standard')
-AND GETDATE() > prod.DiscontinuedDate
+WHERE (prodanddescription.CultureID = 'en' AND prodsubcategory.Name IN ('Road Frames','Mountain Bikes','standard') AND prod.DiscontinuedDate IS NOT NULL AND prod.ProductID = @Product_Id)
+OR (prodanddescription.CultureID = 'en' AND prodsubcategory.Name IN ('Road Frames','Mountain Bikes','standard') AND prod.DiscontinuedDate IS NOT NULL AND prod.ProductID IS NULL)
+OR (prodanddescription.CultureID = 'en' AND prodsubcategory.Name IN ('Road Frames','Mountain Bikes','standard') AND prod.DiscontinuedDate IS NOT NULL AND @Product_Id = 0)
+END
 
 
 /*Ejercicio 2*/
-SELECT 
+GO
+CREATE OR ALTER PROCEDURE UDP_Ejercicio2
+@cutomerID INT
+AS
+BEGIN
+
+SELECT
 ISNULL(person.Title,'') + ' ' + person.FirstName + ' ' + person.LastName AS Cliente,
 YEAR(salesorderheader.OrderDate) AS year,
 (COUNT(*)) AS CantidadComprasTotales,
@@ -111,19 +124,39 @@ AVG(salesorderheader.TotalDue) AS valorPromedio
 FROM [Sales].[Customer] customer INNER JOIN [Person].[Person] person 
 ON customer.PersonID = person.BusinessEntityID INNER JOIN [Sales].[SalesOrderHeader] salesorderheader
 ON customer.CustomerID = salesorderheader.CustomerID
+WHERE customer.CustomerID = @cutomerID OR @cutomerID IS NULL OR @cutomerID = 0
 GROUP BY YEAR(salesorderheader.OrderDate), (ISNULL(person.Title,'') + ' ' + person.FirstName + ' ' + person.LastName), customer.StoreID
 HAVING customer.StoreID IS NULL
 
+END
+
+
 /*Ejercicio 3*/
+GO
+CREATE OR ALTER PROCEDURE UDP_Ejercicio3
+@BusinessEntityID INT
+AS
+BEGIN
+
 SELECT CONCAT(person.FirstName,' ',LastName) AS Cliente,
 COUNT(businessentitycontact.BusinessEntityID) AS Conteo
 FROM [Person].[BusinessEntityContact] businessentitycontact INNER JOIN [Person].[Person] person
 ON businessentitycontact.PersonID = person.BusinessEntityID INNER JOIN [Sales].[Customer] customer
 ON businessentitycontact.PersonID = customer.PersonID
+WHERE businessentitycontact.BusinessEntityID = @BusinessEntityID OR @BusinessEntityID IS NULL OR @BusinessEntityID = 0
 GROUP BY CONCAT(person.FirstName,' ',LastName)
 
+END
+
 /*Ejercicio 4 */
+GO
+CREATE OR ALTER PROCEDURE UDP_Ejercicio4
+@BusinessEntityID INT
+AS
+BEGIN
+
 SELECT 
+store.BusinessEntityID,
 store.Name AS StoreName,
 salesterritory.Name AS Territory,
 CONCAT(person.FirstName,' ',person.LastName) AS Customer
@@ -132,7 +165,27 @@ ON store.BusinessEntityID = businessentitycontact.BusinessEntityID	INNER JOIN [S
 ON store.SalesPersonID = salesperson.BusinessEntityID				INNER JOIN [Sales].[SalesTerritory] salesterritory
 ON salesperson.TerritoryID = salesterritory.TerritoryID				INNER JOIN [Person].[Person] person
 ON businessentitycontact.PersonID = person.BusinessEntityID
+WHERE store.BusinessEntityID = @BusinessEntityID OR @BusinessEntityID IS NULL OR @BusinessEntityID = 0
+
+END
+
 
 /*Ejercicio 5*/
 
 /*Lista de personas Titulo, Nombre, Apellido (En una sola columna titulada como Empleado) y genero que han trabajado mas de la mitad sde sus años de vida en la empresa*/
+GO
+CREATE OR ALTER PROCEDURE UDP_Ejercicio5
+@BusinessEntityID INT
+AS
+BEGIN
+
+SELECT 
+CONCAT(employee.JobTitle,' ',FirstName,' ',LastName) AS Empleado,
+employee.Gender AS Gender
+FROM [Person].[Person] person INNER JOIN [HumanResources].[Employee] employee
+ON person.BusinessEntityID = employee.BusinessEntityID 
+WHERE (((YEAR(GETDATE()) - YEAR(employee.BirthDate)) / 2) <= (YEAR(GETDATE()) - YEAR(HireDate)) AND employee.BusinessEntityID = @BusinessEntityID) 
+OR (((YEAR(GETDATE()) - YEAR(employee.BirthDate)) / 2) <= (YEAR(GETDATE()) - YEAR(HireDate)) AND @BusinessEntityID IS NULL)
+OR (((YEAR(GETDATE()) - YEAR(employee.BirthDate)) / 2) <= (YEAR(GETDATE()) - YEAR(HireDate)) AND @BusinessEntityID = 0)
+
+END
